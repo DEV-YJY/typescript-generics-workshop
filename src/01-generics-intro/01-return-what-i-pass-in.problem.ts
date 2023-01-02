21f743d1d3b4253bd5ceca9a8f0fbc4cf2c79090
YJ You <yyou017@gmail.com>
2023-01-02 17:02:52 +1300

    Complete exercise 01

--- src/01-generics-intro/01-return-what-i-pass-in.problem.ts
+++ src/01-generics-intro/01-return-what-i-pass-in.problem.ts
@@ -1,8 +1,14 @@
 import { Equal, Expect } from "../helpers/type-utils";
 
-const returnWhatIPassIn = (t: unknown) => {
+// defined with the const keyword
+const returnWhatIPassIn = <T>(t: T) => {
   return t;
 };
+
+// defined with the function keyword
+// function returnWhatIPassIn<T>(t: T) {
+//   return t
+// }
 
 const one = returnWhatIPassIn(1);
 const matt = returnWhatIPassIn("matt");
