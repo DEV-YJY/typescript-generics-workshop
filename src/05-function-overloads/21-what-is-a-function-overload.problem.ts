8457509f345893df099842d8980e43fc090344fa
YJ You <yyou017@gmail.com>
2023-02-07 16:27:15 +1300

    Complete exercise 22 again

--- src/05-function-overloads/21-what-is-a-function-overload.problem.ts
+++ src/05-function-overloads/21-what-is-a-function-overload.problem.ts
@@ -3,11 +3,9 @@ import { Equal, Expect } from "../helpers/type-utils";
 /**
  * This time, let's solve this with function overloads!
  */
-function returnWhatIPassIn(t: 1): 1;
-function returnWhatIPassIn(t: "matt"): "matt";
-function returnWhatIPassIn(t: unknown) {
+const returnWhatIPassIn = (t: unknown) => {
   return t;
-}
+};
 
 const one = returnWhatIPassIn(1);
 const matt = returnWhatIPassIn("matt");
